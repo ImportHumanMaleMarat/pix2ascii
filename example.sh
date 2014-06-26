5606cc10d55691af53b098132039bf728a147561
ImportHumanMaleMarat <java.c.csharp@gmail.com>
2014-06-26 04:43:42 +0400

    TEST

--- example.sh
+++ example.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+if [ ! -d "./example_out" ]; then
+	mkdir ./example_out
+fi
 ./bin/pix2ascii -o ./example_out/pop.ascii  ./example_in/pop.gif 0 7 4
 ./bin/pix2ascii -o ./example_out/king.ascii  ./example_in/king.png 4 7 0
 ./bin/pix2ascii -o ./example_out/ghost.ascii ./example_in/ghost.gif 3 0 4 7
